0d221be87fb4fc06b56a39260146f0be6eeecfbe
alexmelo03 <alexmello79@yahoo.com.br>
2020-06-18 05:58:15 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -33,10 +33,8 @@ export class CategoriasPage {
     .subscribe(response => {
       this.items = response;
     },
-    error => {
-      console.log(error);
+    error => {});
     }
-    );
-  }
-
+    
+  
 }
